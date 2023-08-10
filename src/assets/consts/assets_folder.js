06d2de3926009ea187592014e57e2eda59614a35
kvisatchaderah <kvisatchaderah@gmail.com>
2023-08-10 16:06:00 +0300

    -

--- src/assets/consts/assets_folder.js
+++ src/assets/consts/assets_folder.js
@@ -1,1 +1,1 @@
-export default './assets'
+export default 'assets'
--- src/assets/helpers/get_locale_image.js
+++ src/assets/helpers/get_locale_image.js
@@ -1,6 +1,6 @@
 import { assets_folder } from '@consts'
 
 export default (locale_folder, image_name) => {
-  console.log('dfsf')
-  return `${assets_folder}/${locale_folder}/${image_name}`
+  console.log('dsfsdfdsdsf adsf ')
+  return `./${assets_folder}/${locale_folder}/${image_name}`
 }
